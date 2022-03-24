cf6c01631b2d4a9d4d86b2a4b5dba6a5058d5057
Sravanthi1108 <101916647+Sravanthi1108@users.noreply.github.com>
2022-03-24 19:53:44 +0530

    name shrink class added

--- UserMainCode.java
+++ UserMainCode.java
@@ -1,9 +1,20 @@
-package negativestring;
+package nameshrinking;
+import java.util.StringTokenizer;
 
 public class UserMainCode {
-	public static String negativeString(String s) {
-		String n = s.replaceAll(" is ", " is not ");
-		return n;
+	public static String getFormatedString(String s1) {
+		StringBuffer sb = new StringBuffer();
+		StringTokenizer st = new StringTokenizer(s1, " ");
+		String s2 = st.nextToken();
+		String s3 = st.nextToken();
+		String s4 = st.nextToken();
+		sb.append(s4).append(" ");
+		sb.append(s3.substring(0, 1));
+		sb.append(".");
+		sb.append(s2.substring(0, 1));
+		return sb.toString();
+	}
 
-}
+
+
 }
